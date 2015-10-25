f65ee8351307f65e8cddcbc6046c07e852a011c9
Clement Miao <clementmiao@gmail.com>
2015-10-25 14:22:02 -0700

    testing production stuff again

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,25 +1,36 @@
 var path = require('path');
+var webpack = require('webpack');
 var nodeModulesDir = path.resolve(__dirname, 'node_modules');
 
 var config = {
+  devtool: 'source-map',
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/dist/',
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
   module: {
-    loaders: [{
-      test: /\.jsx$/,
-      loader: 'babel',
-      exclude: [nodeModulesDir],
-      loader: 'babel',
-    }, {
-      test: /\.css$/,
-      loader: 'style!css!autoprefixer?browsers=last 2 versions',
-    }]
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'src'),
+        exclude: [nodeModulesDir],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [nodeModulesDir],
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css!autoprefixer?browsers=last 2 versions',
+      },
+    ]
   },
 };
 
